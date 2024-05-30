5e0a07e30d6669813d9be57a7ebf2b654c4edb41
Taksakaa <aprillianravi389@gmail.com>
2024-05-30 11:48:10 +0700

    SummitSync

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:tugas_akhir_tpm/login.dart';
-import 'package:tugas_akhir_tpm/saran.dart';
-import 'package:tugas_akhir_tpm/myapp.dart';
 import 'package:tugas_akhir_tpm/detail_pendaki.dart';
+import 'package:tugas_akhir_tpm/login.dart';
+import 'package:tugas_akhir_tpm/myapp.dart';
+import 'package:tugas_akhir_tpm/saran.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,93 +12,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
-  void _navigateTo(int index) {
-    switch (index) {
-      case 1:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => SaranPage()),
-        );
-        break;
-      case 2:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => MyappPage()),
-        );
-        break;
-      case 3:
-        _showLogoutConfirmation();
-        break;
-    }
-  }
-
-  void _showLogoutConfirmation() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: Colors.white,
-          title: Text(
-            "Logout",
-            style: GoogleFonts.poppins(
-              fontSize: 26,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          content: Text(
-            "Are you sure you want to logout?",
-            style: GoogleFonts.poppins(),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                final prefs = await SharedPreferences.getInstance();
-                await prefs.setBool('loggedIn', false);
-                await prefs.clear();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Text(
-                "Yes",
-                style: GoogleFonts.poppins(
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text(
-                "No",
-                style: GoogleFonts.poppins(
-                  color: Colors.green,
-                ),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Home',
-          style: GoogleFonts.poppins(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Color(0xFF004A3C),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -170,36 +86,328 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(28.0),
+          padding: const EdgeInsets.all(38.0),
           child: Column(
             children: [
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => DetailPendakiPage()),
-                    );
-                  },
-                  child: Text(
-                    'Data Pendaki',
-                    style: GoogleFonts.poppins(color: Colors.white),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Builder(
+                        builder: (context) => IconButton(
+                          icon: Icon(Icons.menu),
+                          onPressed: () => Scaffold.of(context).openDrawer(),
+                          iconSize: 30,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 20.0,
+                        backgroundImage: AssetImage('assets/profile.jpeg'),
+                      ),
+                    ],
                   ),
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      "Find your",
+                      style: GoogleFonts.bitter(
+                          fontSize: 34, fontWeight: FontWeight.w700),
                     ),
-                    backgroundColor: Colors.orange,
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      "perfect ways",
+                      style: GoogleFonts.bitter(
+                          fontSize: 34, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Text(
+                      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour",
+                      style: GoogleFonts.poppins(
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 9.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.375,
+                          height: 220,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                elevation: 5,
+                                child: Image.asset(
+                                  'assets/merbabu.jpg', // Replace with your image
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Positioned(
+                                left: 8,
+                                bottom: 8,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 8, bottom: 0),
+                                      child: Text(
+                                        'Mount Merbabu',
+                                        style: GoogleFonts.bitter(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 0),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 8, bottom: 8),
+                                      child: Text(
+                                        '3,145 meters',
+                                        style: GoogleFonts.bitter(
+                                          fontSize: 12,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 18),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.375,
+                          height: 220,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                elevation: 5,
+                                child: Image.asset(
+                                  'assets/lawu.jpg', // Replace with your image
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Positioned(
+                                left: 8,
+                                bottom: 8,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 8, bottom: 0),
+                                      child: Text(
+                                        'Mount Lawu',
+                                        style: GoogleFonts.bitter(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 0),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 8, bottom: 8),
+                                      child: Text(
+                                        '3,265 meters',
+                                        style: GoogleFonts.bitter(
+                                          fontSize: 12,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: Text(
+                      "Top Destination",
+                      style: GoogleFonts.bitter(
+                          fontSize: 24, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Expanded(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        _buildMountainCard('Mount Merapi', '3,145 meters', 'assets/merapi.jpg'),
+                        _buildMountainCard('Mount Sumbing', '3,676 meters', 'assets/sumbing.jpg'),
+                        _buildMountainCard('Mount Sindoro', '3,726 meters', 'assets/sindoro.jpg'),
+                        _buildMountainCard('Mount Slamet', '2,329 meters', 'assets/slamet.jpg'),
+                        _buildMountainCard('Mount Gede', '3,805 meters', 'assets/gede.jpg'),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildMountainCard(String name, String height, String imagePath) {
+    return Container(
+      width: 150,
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        elevation: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+              ),
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 50  ,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: GoogleFonts.bitter(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    height,
+                    style: GoogleFonts.bitter(
+                      fontSize: 12,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _navigateTo(int index) {
+    switch (index) {
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SaranPage()),
+        );
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyappPage()),
+        );
+        break;
+      case 3:
+        _showLogoutConfirmation();
+        break;
+    }
+  }
+
+  void _showLogoutConfirmation() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          title: Text(
+            "Logout",
+            style: GoogleFonts.poppins(
+              fontSize: 26,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          content: Text(
+            "Are you sure you want to logout?",
+            style: GoogleFonts.poppins(),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                final prefs = await SharedPreferences.getInstance();
+                await prefs.setBool('loggedIn', false);
+                await prefs.clear();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text(
+                "Yes",
+                style: GoogleFonts.poppins(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                "No",
+                style: GoogleFonts.poppins(
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
